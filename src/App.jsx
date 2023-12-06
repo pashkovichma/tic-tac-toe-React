9eda2951087eb7a5aee915141de8ba50bd6fd29a
Maria <pashkovichma@gmail.com>
2023-12-06 11:23:11 +0100

    player name input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="0" />
+        </ol>
+      GAME Board
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
